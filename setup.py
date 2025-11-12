24c078e7ea9d4723467c710cb728034d03a36748
Sudheesh901 <sudheeshkrj@gmail.com>
2025-11-13 00:50:36 +0530

    Dependencies installations done

--- setup.py
+++ setup.py
@@ -1,32 +1,27 @@
-#Can publish this package in pypi
-
 import setuptools
 
-with open("README.md", 'r', encoding="utf-8") as f:
-    long_description=f.read()
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
 
-__version__ ="0.0.0"
+__version__ = "0.0.0"
 
-REPO_NAME= "textsummarisation"
-AUTHOR_USER_NAME= "Sudheesh901"
-SRC_REPO= "textSummarizer"
-AUTHO_EMAIL= "sudheeshkrj@gmail.com"
-
+REPO_NAME = "textsummarisation"
+AUTHOR_USER_NAME = "Sudheesh901"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "sudheeshkrj@gmail.com"
 
 setuptools.setup(
     name=SRC_REPO,
     version=__version__,
     author=AUTHOR_USER_NAME,
-    author_email=AUTHO_EMAIL,
+    author_email=AUTHOR_EMAIL,
     description="A small python package for NLP app",
     long_description=long_description,
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
-        "Bug Tracker": f"https://github.com{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
-    package_dir={"","src"},
+    package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-
-
 )
