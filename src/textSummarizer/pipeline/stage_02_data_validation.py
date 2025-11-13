68839723eed7c19e10df8528b9e4eee27a7d1d44
Sudheesh901 <sudheeshkrj@gmail.com>
2025-11-13 23:01:50 +0530

    Commit after Data Validation

---
+++ src/textSummarizer/pipeline/stage_02_data_validation.py
@@ -0,0 +1,15 @@
+from textSummarizer.config.confuiguration import ConfigurationManager
+from textSummarizer.entity import DataValidationConfig
+from textSummarizer.config.confuiguration import ConfigurationManager
+from textSummarizer.components.data_validation import DataValidation
+from textSummarizer.logging import logger
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+         config=ConfigurationManager()
+         data_validation_config=config.get_data_validation_config()
+         data_validation=DataValidation(config=data_validation_config)
+         data_validation.validate_all_files_exists()
